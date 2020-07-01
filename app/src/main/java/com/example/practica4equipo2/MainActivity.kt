cdc7d0ba01d7ae6cb55f0899564233ae5b42040e
JuanPablo <juanp.escobar@udea.edu.co>
2020-07-01 18:14:16 -0500

    Este es el fragmento correspondiente al área del triángulo

--- app/src/main/java/com/example/practica4equipo2/MainActivity.kt
+++ app/src/main/java/com/example/practica4equipo2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.practica4equipo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.fragment_area_triangulo.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
